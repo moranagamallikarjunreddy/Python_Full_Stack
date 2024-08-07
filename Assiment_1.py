5119e97d07e7e5b114a7e7be0475b4754ddd5945
moranagamallikarjunreddy <nagamallikarjun27@gmail.com>
2024-08-07 15:24:12 +0000

    This is my file

---
+++ Assiment_1.py
@@ -0,0 +1,16 @@
+def  main():
+    firstname=input ("Enter Firstname:")
+    lastname=input("Enter Lastname:")
+    age=int(input("Enter the age:"))
+    if age <0:
+        print(f"your full name is{firstname} {lastname} and please enter valid age")
+
+    elif age <18:
+        print(f"your full name is {firstname} {lastname} and you are a miner")
+    elif age>18 and age<=67:
+        print(f"your full name is {firstname} {lastname} and you are a adlt")  
+    else:
+        print(f"your full name is{firstname} {lastname} and you are a seinor cition ")  
+
+if __name__=="__main__":
+    main()
